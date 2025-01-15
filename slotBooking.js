faabe8853301061eaae1fc3d32c62dc39c7d9074
Shashank <m.shashank12300@gmail.com>
2025-01-15 23:04:55 +0530

    fixed some css code.

--- slotBooking.js
+++ slotBooking.js
@@ -4,19 +4,24 @@ function OnFormSubmit(e) {
   var checkBoxes = document.getElementsByName("services");
   //assuming that theres already an error
   var error = true;
+  var count = 0;
   //looping through all checkboxes
   for (var cb of checkBoxes) {
     //checkbox->checked, no error.
     if (cb.checked) {
       error = false;
-      break;
+      count++;
     }
   }
   //if error, the following code.
   if (error) {
     alert("Select atleast one service");
   } else {
-    var result = confirm("DO you want to proceed further?");
+    var result = confirm(
+      "You have selected " +
+        count +
+        " service(s), click 'ok' to book your appointment."
+    );
     if (result) {
       var mainElement = document.getElementById("mainKaId");
       var newElement = document.getElementById("resultText");
